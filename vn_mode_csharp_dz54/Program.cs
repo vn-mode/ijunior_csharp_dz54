10030cc4a3a5d7cd902af330c4a2276074eb0c5f
VITALII NAGIBIN <98809212+vn-mode@users.noreply.github.com>
2023-07-07 21:18:31 +0300

    ijunior_csharp_dz54

---
+++ vn_mode_csharp_dz54/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Player> players = new List<Player>
+        {
+            new Player { Name = "Игрок 1", Level = 10, Power = 100 },
+            new Player { Name = "Игрок 2", Level = 15, Power = 200 },
+            new Player { Name = "Игрок 3", Level = 20, Power = 150 },
+        };
+
+        var topPlayersByLevel = players.OrderByDescending(p => p.Level).Take(3);
+        Console.WriteLine("Топ 3 игроков по уровню:");
+
+        foreach (var player in topPlayersByLevel)
+        {
+            Console.WriteLine($"Имя: {player.Name}, Уровень: {player.Level}");
+        }
+
+        var topPlayersByPower = players.OrderByDescending(p => p.Power).Take(3);
+        Console.WriteLine("\nТоп 3 игроков по силе:");
+
+        foreach (var player in topPlayersByPower)
+        {
+            Console.WriteLine($"Имя: {player.Name}, Сила: {player.Power}");
+        }
+    }
+}
+
+public class Player
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public int Power { get; set; }
+}
